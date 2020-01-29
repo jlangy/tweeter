1bdd494abf132638156ac2adcece1a655ffc5815
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-29 20:08:30 +0000

    add error handling for tweet submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,31 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
+const MAX_TWEET_LENGTH = 140;
 
 const createTweetElement = function(tweetData){
   const daysAgoCreated = Math.round((Date.now() - new Date(tweetData.created_at)) / 86400000);
@@ -57,6 +58,16 @@ const addSubmitListener = function(){
   $('.add-tweet-form').submit(function(event){
     event.preventDefault();
     const tweetTextInput = $('#tweet-text');
+    const tweetLength = tweetTextInput.val().length;
+    
+    //handle errors
+    if(tweetLength > MAX_TWEET_LENGTH){
+      return alert(`This tweet is more than ${MAX_TWEET_LENGTH} characters. Please be more pithy.`)
+    } else if (tweetLength === 0){
+      return alert('Please enter some text to tweet.');
+    }
+
+    //post tweet and reset
     $.ajax("/tweets", {method: "POST", data: tweetTextInput.serialize()})
       .then(function(){
         tweetTextInput.val('');
@@ -64,7 +75,15 @@ const addSubmitListener = function(){
   });
 }
 
+const loadTweets = function(){
+  $.ajax('/tweets', {method:"GET"})
+    .then(function(data){
+      renderTweets(data);
+    });
+}
+
 $(document).ready(function(){
-  renderTweets(data);
+  // renderTweets(data);
   addSubmitListener();
+  loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('#tweet-text').on('keyup', function(event){
     const textArea = $(this);
     const counter = textArea.siblings('span');
-    const textLength = 140 - textArea.val().length;
+    const textLength = MAX_TWEET_LENGTH - textArea.val().length;
     counter.text(textLength);
     textLength < 0 ? counter.addClass('danger-text') : counter.removeClass('danger-text');
   });
