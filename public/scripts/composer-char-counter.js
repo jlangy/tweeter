22ca9bfaa50d34ed9370637eee9d7e32732b53d7
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-29 03:39:13 +0000

    add article section styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,9 @@
 $(document).ready(() => {
-  $('.new-tweet>form>textarea').on('keyup', function(event){
-    console.log($(this).val().length);
+  $('#tweet-text').on('keyup', function(event){
+    const textArea = $(this);
+    const counter = textArea.siblings('span');
+    const textLength = 140 - textArea.val().length;
+    counter.text(textLength);
+    textLength < 0 ? counter.addClass('danger-text') : counter.removeClass('danger-text');
   });
 });
