9504bc153d73843aac54b9a694882e6ace232936
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-29 23:45:22 +0000

    add go to tweet btn functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,40 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 const MAX_TWEET_LENGTH = 140;
+const NAV_HEIGHT = 120;
 
 const createTweetElement = function(tweetData){
-  console.log(tweetData);
   const daysAgoCreated = Math.round((Date.now() - new Date(tweetData.created_at)) / 86400000);
   const $avatar = $('<span>')
                     .text(tweetData.user.name)
                     .prepend($('<img/>')
-                    .attr({'src':tweetData.user.avatars, "alt":"profile picture"}))
-
+                    .attr({'src':tweetData.user.avatars, "alt":"profile picture"}));
 
   const $header = $('<header>')
                     .append($avatar)
@@ -71,7 +46,7 @@ const renderTweets = function(tweetData){
 }
 
 const addSubmitListener = function(){
-  $('.add-tweet-form').submit(function(event){
+  $('#add-tweet-form').submit(function(event){
     event.preventDefault();
     const tweetTextInput = $('#tweet-text');
     const tweetLength = tweetTextInput.val().length;
@@ -99,7 +74,22 @@ const loadTweets = function(){
     });
 }
 
+const setScrollListener = function (event) {
+  let location = $(window).scrollTop();
+  let formLocation = $('#compose-tweet').offset().top - NAV_HEIGHT;
+  if(Math.abs(location - formLocation) > 1){
+    location = formLocation;
+  } else {
+    location = $('#articles-container').offset().top - NAV_HEIGHT;
+  }
+  $('html, body').stop().animate({
+    'scrollTop': location
+  }, 300, 'swing');
+}
+
 $(document).ready(function(){
+  $(window).scrollTop(0);
   addSubmitListener();
   loadTweets();
+  $('#scroll-btn').on('click', setScrollListener);
 });
